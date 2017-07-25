adf0de458a0712a1242902442b61b0f1d4a33151
Allison Rainey <allisonsrainey@gmail.com>
2017-07-25 19:09:58 -0400

    Working Craps game

--- CrapsGUI.java
+++ CrapsGUI.java
@@ -16,16 +16,6 @@ public class CrapsGUI extends JFrame
   // create secure random number generator for use in method rollDice
   private static final SecureRandom randomNumbers = new SecureRandom();
 
-  // enum type with constants that represent the game status
-  private enum Status {CONTINUE, WON, LOST};
-
-  // constants that represent common rolls of the dice
-  private static final int SNAKE_EYES = 2;
-  private static final int TREY = 3;
-  private static final int SEVEN = 7;
-  private static final int YO_LEVEN = 11;
-  private static final int BOX_CARS = 12;
-
   // GUI fields
   private JLabel jlabel1, jlabel2, jlabel3, jlabel4, gameStatusLabel;
   private JTextField jtext1, jtext2, jtext3, jtext4;
@@ -66,24 +56,20 @@ public class CrapsGUI extends JFrame
     roll.addActionListener(bHandler);
     playAgain.addActionListener(bHandler);
 
-    // Unnecessary? No user action on the text fields
-    /*TextFieldHandler handler = new TextFieldHandler();
-    jtext1.addActionListener(handler);
-    jtext2.addActionListener(handler);
-    jtext3.addActionListener(handler);
-    jtext4.addActionListener(handler); */
   }
 
   private class ButtonHandler implements ActionListener
   {
+    private int myPoint = 0;   // point if no win or loss on first roll
+    private int sumOfDice = 0;
+
     @Override
     public void actionPerformed(ActionEvent e)
     {
       if(e.getSource() == roll)
       {
-        CrapsGUI game = new CrapsGUI();
-        game.rollDice();
-        //game.playCraps();
+        ButtonHandler game = new ButtonHandler();
+        myPoint = game.playCraps(myPoint);
       }
 
       if(e.getSource() == playAgain)
@@ -92,77 +78,61 @@ public class CrapsGUI extends JFrame
         jtext2.setText("");
         jtext3.setText("");
         jtext4.setText("");
-        // Clear sum and points counters
+        myPoint = 0;
+        sumOfDice = 0;
+        gameStatusLabel.setText("");
 
       }
     }
-  }
 
-
-
-  // plays one game of craps
-  public void playCraps()
-  {
-    int myPoint = 0;   // point if no win or loss on first roll
-    Status gameStatus;  // can contain CONTINUE, WON or LOST
-
-    int sumOfDice = rollDice();  // first roll of the dice
-
-    // determine game status and point based on first roll
-    switch(sumOfDice)
+    public int playCraps(int myPoint)
     {
-      case SEVEN:    // win with 7 on the first roll
-      case YO_LEVEN: // win with 11 on the first roll
-      gameStatus = Status.WON;
-      break;
-      case SNAKE_EYES:  // lose with 2 on first roll
-      case TREY:        // lose with 3 on first roll
-      case BOX_CARS:    // lose with 12 on first roll
-      gameStatus = Status.LOST;
-      break;
-      default:     // did not win or lose so remember point
-      gameStatus = Status.CONTINUE;   // game is not over
-      myPoint = sumOfDice;     // remember the point
-      jtext4.setText(String.valueOf(myPoint));
-      break;
-    }
-    // while game is not complete
-    while(gameStatus == Status.CONTINUE)  // not WON or LOST
-    {
-      sumOfDice = rollDice();   // roll dice again
+      int die1 = 1 + randomNumbers.nextInt(6); // first die roll
+      int die2 = 1 + randomNumbers.nextInt(6); // second die roll
 
-      // determine game status
-      if(sumOfDice == myPoint) // win by making point
-        gameStatus = Status.WON;
+      sumOfDice = die1 + die2; // sum of die values
+
+      // display the results of this roll
+      jtext1.setText(String.valueOf(die1));
+      jtext2.setText(String.valueOf(die2));
+      jtext3.setText(String.valueOf(sumOfDice));
+
+      if(myPoint == 0)
+      {
+        if(sumOfDice == 7 || sumOfDice == 11)
+        {
+          gameStatusLabel.setText("Player wins!");
+        }
+        else if(sumOfDice == 2 || sumOfDice == 3 || sumOfDice == 12)
+        {
+          gameStatusLabel.setText("Player loses!");
+        }
+        else     // did not win or lose so remember point
+        {
+          myPoint = sumOfDice;
+          jtext4.setText(String.valueOf(myPoint));
+          gameStatusLabel.setText("Roll Again");
+        }
+      }
       else
-      if(sumOfDice == SEVEN)   // lose by rolling 7 before point
-        gameStatus = Status.LOST;
+      {
+        if(sumOfDice == myPoint) // win by making point
+        {
+          gameStatusLabel.setText("Player wins!");
+        }
+        else if(sumOfDice == 7)   // lose by rolling 7 before point
+        {
+          gameStatusLabel.setText("Player loses!");
+        }
+        else
+        {
+          gameStatusLabel.setText("Roll Again");
+        }
+      }
+      return myPoint;
     }
-
-    // display won or lost message
-    if(gameStatus == Status.WON)
-      gameStatusLabel.setText("Player wins!");
-    else
-      gameStatusLabel.setText("Player loses!");
   }
-
-  // roll dice, calculate sum and display results
-  public int rollDice()
-  {
-    // pick random die values
-    int die1 = 1 + randomNumbers.nextInt(6); // first die roll
-    int die2 = 1 + randomNumbers.nextInt(6); // second die roll
-
-    int sum = die1 + die2; // sum of die values
-
-    // display the results of this roll
-    jtext1.setText(String.valueOf(die1));
-    jtext2.setText(String.valueOf(die2));
-    jtext3.setText(String.valueOf(sum));
-
-    return sum;
-  }
-
+  
   public static void main(String[] args)
   {
     CrapsGUI frame = new CrapsGUI();
@@ -171,4 +141,5 @@ public class CrapsGUI extends JFrame
     frame.setSize(800, 400);
     frame.setVisible(true);
   }
+
 }
